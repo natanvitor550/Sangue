546423e47e8c55b688450bac6c382af5259f7836
Natan <natan_v_barbosa@estudante.sesisenai.org.br>
2026-04-13 11:28:10 -0300

    add- Criação de todo layout do site

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("FormSangue").addEventListener("submit", function (e) {
+    e.preventDefault();
+    let nome = document.getElementById("nome").value;
+    let email = document.getElementById("email").value;
+    let
+
+
+
+
+})
